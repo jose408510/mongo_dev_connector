2184b7cd19ab029c9decd20a929ae24b7897481b
Jose Castellanos <josecastellanos@Joses-MacBook-Pro-2.local>
2018-06-26 18:57:02 -0700

    routes

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = { 
+    mongoURI: 
+    'mongodb://jose:sanjo408@ds249545.mlab.com:49545/mongoapp23'
+}
--- server.js
+++ server.js
@@ -1,11 +1,30 @@
-const express = require("express")
-const app = express();
+const express = require("express");
+const mongoose = require("mongoose");
 
-app.get("/", (req,res) => console.log("hello"))
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const post = require('./routes/api/post')
+
+const db = require('./config/keys').mongoURI;
+
+const app = express();
+// connect to mongodb
+mongoose
+.connect(db)
+.then(() => console.log("'mongoDb Connected")) 
+.catch(err => console.log(err))
+
+
+
+app.get("/", (req,res) => res.send("hello"))
+
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/post', post);
+
 
 
 const port = process.env.PORT || 5000;
-
-
 app.listen(port, () => console.log(`Server runing on  ${port}`))
 
